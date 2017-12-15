866852c9cfa0a085da4386b32caf10c179165168
Robins Gupta <robinskumar73@gmail.com>
2017-12-15 19:18:03 +0530

    Updating code

--- backend/loginWithOtp.js
+++ backend/loginWithOtp.js
@@ -9,7 +9,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
     const _ = require('lodash');
 
     const addPasswordlessLogin = function (msg91Config) {
-        const {method, requestOtp, retryOtp, enable} = msg91Config.login.mobile;
+        const {method, requestOtp, retryOtp, enable, oneInstanceLogin} = msg91Config.login.mobile;
         const message = msg91Config.credentials.defaultMessage || 'Otp for your request is {{otp}}, please do not share it with anybody';
         const sendOTP = new SendOtp(msg91Config.credentials.authKey, message);
         const expiryTime = msg91Config.credentials.expiryInMinutes || "2"; //by default it has been set up to 2
@@ -48,7 +48,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
             };
 
             User[method] = function(number, otp, callback){
-                passwordLessMethod(msg91Config, number, otp, sendOTP)
+                passwordLessMethod(msg91Config, number, otp, sendOTP, oneInstanceLogin)
                     .then(function (data) {
                         callback(null, data);
                     })
@@ -123,7 +123,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
      * @param otp
      * @param sendOTP
      */
-    const passwordLessMethod = function(msg91Config, number, otp, sendOTP){
+    const passwordLessMethod = function(msg91Config, number, otp, sendOTP, oneInstanceLogin){
         return new Promise(function (resolve, reject) {
             if(number && otp){
                 number = formatNumber(number);
@@ -132,7 +132,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
                         reject(error);
                     }else if(data.type === 'success') {
                         var User = databaseObj.User;
-                        createUserOrLogin(number, User, msg91Config)
+                        createUserOrLogin(number, User, msg91Config, oneInstanceLogin)
                             .then(function (accessToken) {
                                 resolve(accessToken);
                             })
@@ -258,7 +258,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
      * @param msg91Config {{}}
 
      */
-    var createUserOrLogin = function(number, User, msg91Config){
+    var createUserOrLogin = function(number, User, msg91Config, oneInstanceLogin){
         return new Promise(function (resolve, reject) {
             var defaultError = new Error('login failed');
             defaultError.statusCode = 401;
@@ -285,7 +285,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
                                 console.error(err);
                                 return reject(defaultError);
                             }else{
-                                updateAccessTokenModel(userObj)
+                                updateAccessTokenModel(userObj, oneInstanceLogin)
                                     .then(function (accessToken) {
                                         resolve(accessToken);
                                     })
@@ -299,7 +299,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
                         if(user){
                             //Just provide login
                             updateAccessTokenModel(user)
-                                .then(function (accessToken) {
+                                .then(function (accessToken, oneInstanceLogin) {
                                     resolve(accessToken);
                                 })
                                 .catch(function (error) {
@@ -325,18 +325,39 @@ module.exports = function( server, databaseObj, helper, packageObj) {
      * Update the user accessToken data
      * @param userInstance
      */
-    var updateAccessTokenModel = function(userInstance){
+    var updateAccessTokenModel = function(userInstance, oneInstanceLogin){
         return new Promise(function (resolve, reject) {
-            userInstance.createAccessToken(31536000, function(error, token) {
-                if (error) {
-                    console.error(error);
-                    return reject(error);
+            new Promise(function(resolve, reject){
+                if(oneInstanceLogin){
+                    AccessToken = databaseObj.AccessToken;
+                    AccessToken.destroyAll({
+                        userId: userInstance.id
+                    })
+                    .then(function(){
+                        resolve();
+                    })
+                    .catch(function(error){
+                        reject(error);
+                    })
                 }else{
-                    token.__data.user = userInstance;
-                    //console.log(token);
-                    resolve(token);
+                    resolve();
                 }
-            }); //createAccessToken
+            })
+            .then(function(){
+                userInstance.createAccessToken(31536000, function(error, token) {
+                    if (error) {
+                        console.error(error);
+                        return reject(error);
+                    }else{
+                        token.__data.user = userInstance;
+                        //console.log(token);
+                        resolve(token);
+                    }
+                }); //createAccessToken
+            })
+            .catch(function(error){
+                reject(error);
+            });
         });
     };
 
